ad4bbaf2f6972644221d8512a09ac0232c1be7ef
umap341 <80568164+umap341@users.noreply.github.com>
2022-09-27 13:21:40 -0700

    Create function.c

---
+++ examples/algorithm/function.c
@@ -0,0 +1,22 @@
+#include "examples/algorithm/function.h"
+
+size_t function(const size_t input)
+{
+  size_t output = (input + 1);
+  return output;
+}
+
+/*
+var R = 6371e3; // metres
+var φ1 = lat1.toRadians();
+var φ2 = lat2.toRadians();
+var Δφ = (lat2-lat1).toRadians();
+var Δλ = (lon2-lon1).toRadians();
+
+var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+var d = R * c;
+*/
