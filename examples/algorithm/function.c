885d877b1ff8d150cf3fa40db4a119612bcfe70d
umap341 <80568164+umap341@users.noreply.github.com>
2023-01-11 20:39:08 -0800

    Update function.c

--- examples/algorithm/function.c
+++ examples/algorithm/function.c
@@ -1,22 +1,39 @@
-#include "examples/algorithm/function.h"
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+#include "function.h"
 
-size_t function(const size_t input)
+#define M_PI 3.14159265358979323846
+// #define ER (6372797.560856)
+#define RAD(degrees) (degrees * (M_PI / 180.0))
+
+long double function(long double lat1, long double long1,long double lat2, long double long2)
 {
-  size_t output = (input + 1);
-  return output;
+    long double dLa1, dLo1, dLa2, dLo2;
+
+    dLa1 = RAD(lat1);
+    dLo1 = RAD(long1);
+    dLa2 = RAD(lat2);
+    dLo2 = RAD(long2);
+
+    long double dlong = dLo2 - dLo1;
+    long double dlat = dLa2 - dLa1;
+
+ 
+    long double ans = pow(sin(dlat / 2), 2) +
+                    cos(dLa1) * cos(dLa2) *
+                    pow(sin(dlong / 2), 2);
+ 
+    ans = 2 * asin(sqrt(ans));
+ 
+    // Radius of Earth in
+    // Kilometers, R = 6371
+    // Use R = 3956 for miles
+    // double R = 6371;
+    double R = 6371;
+     
+    // Calculate the result
+    ans = ans * R;
+    return ans;
 }
-
-/*
-var R = 6371e3; // metres
-var φ1 = lat1.toRadians();
-var φ2 = lat2.toRadians();
-var Δφ = (lat2-lat1).toRadians();
-var Δλ = (lon2-lon1).toRadians();
-
-var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-        Math.cos(φ1) * Math.cos(φ2) *
-        Math.sin(Δλ/2) * Math.sin(Δλ/2);
-var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-var d = R * c;
-*/
