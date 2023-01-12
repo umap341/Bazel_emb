b3b3ced30bb2c92936e0a3655662f818a017f2bf
umap341 <80568164+umap341@users.noreply.github.com>
2023-01-11 20:39:28 -0800

    Update function.h

--- examples/algorithm/function.h
+++ examples/algorithm/function.h
@@ -1,7 +1,5 @@
-#pragma once
+
 #include <stddef.h>
 #include "stdint.h"
 
-size_t function(const size_t input);
-
-/// \}
+long double function(const long double lat1,const long double long1, const long double lat2, const long double long2);
