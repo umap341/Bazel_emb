9d4ce7391369a78451eb7dd98ced20402312939c
umap341 <80568164+umap341@users.noreply.github.com>
2023-01-11 20:39:50 -0800

    Update main.c

--- examples/application/main.c
+++ examples/application/main.c
@@ -1,18 +1,41 @@
-#include <stdio.h>
-#include "examples/algorithm/function.h"
+// #include "C:\Users\patilu3\Bazel_Projects\bazel-build-sample\examples\algorithm\function.h"
 
-/// Application overview.
+#include "examples/algorithm/function.h"
+//#include "function.h"
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+
 int main()
 {
-  // Read the input from the user.
-  size_t input = 0;
-  printf("> ");
-  scanf("%zu", &input);
+//  while(1)
+//  {
+      // Read the input from the user.
+      double lat1 = 0.00;
+      double long1 = 0.00;
+      double lat2 = 0.00;
+      double long2 = 0.00;
 
-  // Calculate the output.
-  size_t output = function(input);
+      clock_t start, end;
+      double cpu_time_used;
 
-  // Return the output to the user.
-  printf("%zu\n", output);
-  return output;
+      start = clock();
+      printf("> ");
+      scanf("%lf, %lf, %lf, %lf", &lat1, &long1, &lat2, &long2);
+      
+      // Calculate the output.
+     long double output = function(lat1,long1,lat2,long2);
+
+      end = clock();
+      cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+      // Return the output to the user.
+      printf("%.2Lf,%.2f", output, cpu_time_used);
+      // printf("%.2f", cpu_time_used);
+//  }
+      return (int)output;
 }
+
+
+
